7944ae882f80bd92d90f7f849631059e33dc89e1
Paul Krogmeier <pkrogmei@purdue.edu>
2016-11-08 18:27:31 -0500

    first commit

---
+++ music.py
@@ -0,0 +1,20 @@
+import webbrowser as wb
+import requests
+from lxml import html
+import re
+import pprint as pp
+
+if __name__ == "__main__":
+    pp.pprint("---GETting HTML from npr.org---")
+    resp = requests.get(
+        "https://web.archive.org/web/20161108001849/http://www.npr.org/programs/all-things-considered/")
+    tree = html.fromstring(resp.content)
+    # get titles
+    song_wrappers = tree.find_class("song-meta-wrap")
+    queries = []
+    for wrapper in song_wrappers:
+        queries.append(" ".join((wrapper.text_content()).split()))
+
+    for query in queries:
+        wb.open_new("http://youtube.com/results?search_query=" + query)
+    pp.pprint("---Check your browser batch---")
